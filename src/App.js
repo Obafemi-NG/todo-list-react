d6aad23a34f26164d17bb9d8529e4c9652cb9a84
Obafemi <72113368+Obafemi-NG@users.noreply.github.com>
2021-06-01 12:50:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoList from "./components/TodoList";
+import TodoInput from "./components/TodoInput";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    items: [{id: 1, title: 'wake up'}],
+    id: uuidv4(),
+    item: "",
+    editItem: false,
+  };
+  handleChange = (e) => {
+    console.log("handle change");
+  };
+  handleSubmit = (e) => {
+    console.log("handle submit");
+  };
+  handleClearList = () => {
+    console.log("handle clear list");
+  };
+  handleEdit = (id) => {
+    console.log(`handle delete ${id}`);
+  };
+  handleRemove = (id) => {
+    console.log(`handle delete ${id}`);
+  };
+  render() {
+    // console.log(this.state);
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h2 className="text-center text-capitalize">todo input</h2>
+            <TodoInput
+            item={this.state.item}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            editItem={this.state.editItem}
+          />
+          <h2 className = 'text-center text-capitalize' > todo list</h2>
+          <TodoList
+            items={this.state.items}
+            handleEdit={this.handleEdit}
+            handleRemove={this.handleRemove}
+            handleClearList={this.handleClearList}
+          />
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/TodoInput.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+export default class TodoInput extends Component {
+  render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
+    return (
+      <div className="card card-body my-3">
+        <form>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book fa-2x" />
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitalize"
+              placeholder="add a todo item"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+        </form>
+        <button
+          type="submit"
+          className="btn btn-block btn-primary text-uppercase mt-3"
+          onClick={handleSubmit}
+        >
+          add item
+        </button>
+      </div>
+    );
+  }
+}
